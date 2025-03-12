896826fde15250874d0bc0ef57bff7eda11a5b56
Торканевский Давид 115 <75617199+Hacker979@users.noreply.github.com>
2025-03-12 09:38:22 +0200

    Add files via upload

---
+++ employee.sql
@@ -0,0 +1,30 @@
+create table employee (
+	id INT,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	gender VARCHAR(6) NOT NULL,
+	email VARCHAR(150),
+	date_of_birth DATE NOT NULL,
+	country_of_birth VARCHAR(50) NOT NULL
+);
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (1, 'Winona', 'Nestle', 'Female', 'wnestle0@icq.com', '12/9/2024', 'Portugal');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (2, 'Salem', 'Miners', 'Male', 'sminers1@wunderground.com', '10/22/2024', 'Georgia');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (3, 'Feliza', 'Tamlett', 'Female', 'ftamlett2@businessweek.com', '1/14/2025', 'China');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (4, 'Lorette', 'Godon', 'Female', 'lgodon3@bbb.org', '2/26/2025', 'Portugal');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (5, 'Tasia', 'Victoria', 'Female', 'tvictoria4@irs.gov', '10/22/2024', 'China');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (6, 'Margret', 'Coultous', 'Female', 'mcoultous5@amazon.com', '8/16/2024', 'Indonesia');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (7, 'Zorah', 'Snoding', 'Female', 'zsnoding6@epa.gov', '7/9/2024', 'Philippines');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (8, 'Levon', 'Gullberg', 'Male', 'lgullberg7@abc.net.au', '9/11/2024', 'Cameroon');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (9, 'Karlotte', 'Timbrell', 'Female', 'ktimbrell8@shinystat.com', '6/23/2024', 'Poland');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (10, 'Clayton', 'Carless', 'Male', 'ccarless9@xrea.com', '10/21/2024', 'Canada');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (11, 'Floria', 'Custard', 'Female', 'fcustarda@merriam-webster.com', '3/10/2025', 'Cuba');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (12, 'Bunnie', 'Souttar', 'Female', 'bsouttarb@ycombinator.com', '3/9/2025', 'Russia');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (13, 'Krissie', 'Gleadhall', 'Female', 'kgleadhallc@spiegel.de', '1/12/2025', 'Spain');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (14, 'Desiree', 'Gammell', 'Female', 'dgammelld@soup.io', '5/14/2024', 'Indonesia');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (15, 'Kora', 'Smithies', 'Female', 'ksmithiese@cnn.com', '8/7/2024', 'China');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (16, 'Cherrita', 'Du Hamel', 'Female', 'cduhamelf@nature.com', '7/19/2024', 'Indonesia');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (17, 'Reagan', 'Penk', 'Male', 'rpenkg@amazon.de', '5/17/2024', 'Philippines');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (18, 'Noell', 'Arni', 'Genderfluid', 'narnih@bbc.co.uk', '12/31/2024', 'United States');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (19, 'Suzy', 'Hatry', 'Female', 'shatryi@wordpress.org', '3/21/2024', 'New Zealand');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (20, 'Myles', 'Crews', 'Male', 'mcrewsj@stumbleupon.com', '2/7/2025', 'United States');
+insert into employee (id, first_name, last_name, gender, email, date_of_birth, country_of_birth) values (21, 'Kalvin', 'Tort', 'Male', 'ktortk@deviantart.com', '12/23/2024', 'Poland');
